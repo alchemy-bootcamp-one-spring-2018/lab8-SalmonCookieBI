0732e079134025a4b53753c13f774d239bdbdb88
Erik Withak <ErikAanestad@gmail.com>
2018-05-10 14:43:57 -0700

    cleaned tthings up

--- js/object.js
+++ js/object.js
@@ -10,19 +10,15 @@ class Cookie {
     }
 
     cookieData() {
-        this.cookies = [];
-        this.cookies.push(this.location);
+        this.cookies = [this.location];
         for(let i = 0; i < 15; i++) {
             let tempAvg = Math.ceil(((Math.random() * parseInt(this.max) - parseInt(this.min)) + parseInt(this.min)) * this.avg);
             console.log('max', this.max, 'min', this.min, 'avg', this.avg, 'temp', tempAvg);
             this.cookies.push(tempAvg);
         }
-        
-        // console.log('cookies are', this.cookies);
     }
 
     renderMe() {
-        // console.log('should render an object', tempArray);
         render(this.cookies, 'body', 'body-data');
     }
 }
