88816e2847c6abc7959acbbd28004fd8c83a9764
Arthur Jen <arthurljen@gmail.com>
2018-05-10 13:59:58 -0700

    mock data generator complete

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function randomCustomerAmount(array) {
     for(let i = 0; i < array.length; i++) {
         for(let j = 0; j < 15; j++) {
             array[i].customers[j] = getRandomInt(array[i].min, array[i].max);
-            array[i].cookies[j] = array[i].customers[j] * array.avg;
+            array[i].cookies[j] = Math.round(array[i].customers[j] * array[i].avg);
         }
     }
 }
