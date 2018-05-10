4ea3e9e90c8c9bd94dd7e1b2f2ed8f550e5eac12
KLPainter <KLPainter@gmail.com>
2018-05-10 15:08:59 -0700

    completed pseudocode

---
+++ functions.js
@@ -0,0 +1,16 @@
+// Build a location class
+
+
+    // build a function in location class that calculates cookies for each hour of the day
+    // For each hour of the day
+        // find number of customers (a random number between the min and max customers)
+        // multiply number of customer by cookies per sale
+        // also keep track of total cookies per location
+
+
+// Create array of locations
+
+
+// Add new row function which takes array of data and creates a row and adds it to a parent.
+
+
