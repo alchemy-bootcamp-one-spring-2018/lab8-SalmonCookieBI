8629afb6a30e6511b71e0bcf39c2d7964e67e6c0
MariahAdams <mariah.adams23@yahoo.com>
2018-05-11 11:46:09 -0700

    tweaked time and random customer functions, setup started for class CookieShop

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /* globals cookieShops */
 'use strict';
 
-
-function getRandomCustomer(min, max){
+//get inclusive random customer count; source: MDN
+function getRandomIntInclusive(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
@@ -21,8 +21,10 @@ function tableHeaderTime(){
     th.textContent = 'LOCATIONS';
     row.appendChild(th);
     for(var i = 6; i < 21; i++){
-        if(i <= 12){
+        if(i < 12){
             hours.push(i + ':00 am');
+        } else if(i === 12) {
+            hours.push(i + ':00 pm');
         } else {
             hours.push((i - 12) + ':00 pm');
         }
@@ -44,15 +46,13 @@ function tableBodyCalculate(){
     for(var i = 0; i < cookieShops.length; i++){
         //create tr
         var cookieTotals = 0;
-        // var dailyTotals = 0;
         var row = document.createElement('tr');
         attachTrElements.appendChild(row);
-        // row.setAttribute('id', 'row-' + i);
         var StoreName = document.createElement('td');
         StoreName.textContent = cookieShops[i].location;
         row.appendChild(StoreName);
         for(var j = 0; j < hours.length; j++){
-            var cookieSales = Math.floor(getRandomCustomer(cookieShops[i].minCustomer, cookieShops[i].maxCustomer) * cookieShops[i].avgPerCustomer);
+            var cookieSales = Math.floor(getRandomIntInclusive(cookieShops[i].minCustomer, cookieShops[i].maxCustomer) * cookieShops[i].avgPerCustomer);
             cookieTotals += cookieSales;
             var newTdElement = document.createElement('td');
             var totalElement = document.createElement('td');
@@ -62,16 +62,8 @@ function tableBodyCalculate(){
             console.log(cookieTotals);
             row.appendChild(newTdElement);
         }
-        // dailyTotals += cookieSales;
         row.appendChild(totalElement);
     }
-    // var dailyCookieTotals = [];
-    // // console.log('daily cookie totals', dailyCookieTotals);
-    // for(var k = 0; k < hours.length; k++) {
-    //     locationTotals += dailyTotals;
-    //     dailyCookieTotals.push(locationTotals);
-    //     // console.log('location totals', locationTotals);
-    // }
 }
 
 var hourlyTotals = 'total';
