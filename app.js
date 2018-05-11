b959a969ec01c89716edd38e6f2f10d85486a6c9
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-05-10 17:01:18 -0700

    Add punctuation for store names and improve table styling

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 let airport = new StoreData('PDX Airport', 23, 65, 6.3);
 let pioneer = new StoreData('Pioneer Square', 3, 24, 1.2);
-let powells = new StoreData('Powells', 11, 38, 3.7);
-let stjohns = new StoreData('St Johns', 20, 38, 2.3);
+let powells = new StoreData('Powell\'s', 11, 38, 3.7);
+let stjohns = new StoreData('St. John\'s', 20, 38, 2.3);
 let waterfront = new StoreData('Waterfront', 2, 16, 4.6);
 
 let storeArray = [airport, pioneer, powells, stjohns, waterfront];
