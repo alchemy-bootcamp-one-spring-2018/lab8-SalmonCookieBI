9a62a9cbde31b75ca76bf7cfaa0925a6d92f4a22
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-05-12 12:54:43 -0700

    Reorganize file structure and continue to add more HTML and styling

--- js/app.js
+++ js/app.js
@@ -1,12 +1,6 @@
 'use strict';
-/* globals StoreData */
+/* globals StoreData airport, pioneer, powells, stjohns, waterfront */
 /* exported addNewStore */
-
-let airport = new StoreData('PDX Airport', 23, 65, 6.3);
-let pioneer = new StoreData('Pioneer Square', 3, 24, 1.2);
-let powells = new StoreData('Powell\'s', 11, 38, 3.7);
-let stjohns = new StoreData('St. John\'s', 20, 38, 2.3);
-let waterfront = new StoreData('Waterfront', 2, 16, 4.6);
 
 let storeArray = [airport, pioneer, powells, stjohns, waterfront];
 
