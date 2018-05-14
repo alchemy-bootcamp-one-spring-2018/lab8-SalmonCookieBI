ff1249adc37782605564f3fe26b50cd83343b411
Chrisgolden77 <chrgolden@gmail.com>
2018-05-14 10:22:11 -0700

    fixing average per customer value

--- JavaScript/objects.js
+++ JavaScript/objects.js
@@ -17,11 +17,11 @@ class Salmon {
     }
 }
 
-let airport = new Salmon('Airport', 20, 100, 60);
-let pioneerSquare = new Salmon('Pioneer Square', 4, 80, 42);
-let powells = new Salmon('Powell\'s', 8, 98, 53);
-let stJohns = new Salmon('St. John', 3, 5, 4);
-let division = new Salmon('Division', 8, 74, 41);
+let airport = new Salmon('Airport', 20, 100, 2);
+let pioneerSquare = new Salmon('Pioneer Square', 4, 80, 3);
+let powells = new Salmon('Powell\'s', 8, 98, 1);
+let stJohns = new Salmon('St. John', 3, 5, 1);
+let division = new Salmon('Division', 8, 74, 2);
 let locationsArray = [airport, pioneerSquare, powells, stJohns, division];
 let timesArray = ['5a.m.', '6a.m.', '7a.m.', '8a.m.', '9a.m.', '10a.m.', '11a.m', '12p.m.', '1p.m.', '2p.m.', '3p.m'];
 
